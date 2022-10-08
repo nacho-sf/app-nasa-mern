f305a619116aa7e8351ef452189859294df5e522
Nacho-sf <serranof.ignacio@gmail.com>
2022-10-08 13:22:38 +0200

    error404

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const landRouter = require('./routes/landRoutes')
+const userRouter = require('./routes/userRoutes')
+
+const manage404 = require('./middlewares/error404')
+
 const app = express();
 const port = 3000;
 
@@ -7,6 +12,8 @@ app.use(express.json());
 
 app.use("/api/astronomy/landings", landRouter);
 app.use("/api/users", userRouter);
+
+app.use(manage404);
 
 app.listen(port, () => {
     console.log(`El servidor funciona en el puerto ${port}`);
