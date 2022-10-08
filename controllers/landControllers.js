2b8757838186874c2f856864f1bf8dfd6e2c88a7
Nacho-sf <serranof.ignacio@gmail.com>
2022-10-08 21:21:11 +0200

    getbyminmass_undone

--- controllers/landControllers.js
+++ controllers/landControllers.js
@@ -2,13 +2,29 @@ const landModel = require('../models/landModels')
 
 const getLanding = async (req, res) => {
 
-    try {
-        const landings = await landModel.getAllLandings();
-        res.status(200).json(landings);
+    let minMass = req.query.minimum_mass;
+    const minimum_mass = parseInt(minMass)
+
+    if (minimum_mass) {
+        try {
+            const landByMinMass = await landModel.getLandsByMinMass(minimum_mass);
+            res.status(200).json(landByMinMass);
+        }
+        catch (err) {
+            console.log(`Error: ${err.stack}`);
+            res.status(404).json({ "Message": "Landing not found" });
+        }
     }
-    catch (error) {
-        console.log(`Error: ${error.stack}`);
-        res.status(404).json({"Message": "Landings not found"});
+
+    else {
+        try {
+            const landings = await landModel.getAllLandings();
+            res.status(200).json(landings);
+        }
+        catch (error) {
+            console.log(`Error: ${error.stack}`);
+            res.status(404).json({"Message": "Landings not found"});
+        }
     }
 }
 
--- models/landModels.js
+++ models/landModels.js
@@ -1,5 +1,19 @@
 const landSchema = require('../schemas/landSchema')
 
+const getLandsByMinMass = async (minimum_mass) => {
+    try {
+        const landsByMinMass = await landSchema.find({mass: {$gt:minimum_mass}});
+        console.log(typeof minimum_mass);
+        console.log(minimum_mass);
+        console.log(landsByMinMass);
+        return landsByMinMass; 
+    }
+    catch(err){
+        console.error(err);
+    }
+};
+
+
 const getAllLandings = async () => {
     try{
         const allLandings = await landSchema.find({},"-_id");
@@ -9,12 +23,12 @@ const getAllLandings = async () => {
     catch(err){
         console.error(err);
     }
-}
+};
 
 
 const landModels = {
+    getLandsByMinMass,
     getAllLandings
-    
 }
 
 module.exports = landModels
