494dd529841b2d5b29128709f6a98d30293354be
Nacho-sf <serranof.ignacio@gmail.com>
2022-10-09 09:30:04 +0200

    files_structure

---
+++ client/src/components/Main/Landings/Landings.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Landings extends Component {
+  render() {
+    return <div>Landings</div>;
+  }
+}
+
+export default Landings;
---
+++ client/src/components/Main/Neas/Neas.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Neas extends Component {
+  render() {
+    return <div>Neas</div>;
+  }
+}
+
+export default Neas;
