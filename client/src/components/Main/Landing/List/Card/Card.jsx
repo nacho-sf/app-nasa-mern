abaaec3f11c5cf87a6dfbb3f0d47b8c323820275
Nacho-sf <serranof.ignacio@gmail.com>
2022-10-15 23:14:14 +0200

    get_landings

--- client/src/components/Main/Landing/List/Card/Card.jsx
+++ client/src/components/Main/Landing/List/Card/Card.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Card() {
+function Card(props) {
+
+  const [landing, setLanding] = useState(props.value);
+
   return (
     <div className='card'>
+      <p>{landing.name}</p>
+      <p>{landing.id}</p>
+      <p>{landing.recclass}</p>
+      <p>{landing.mass}</p>
       
     </div>
   )
--- client/src/components/Main/Landing/List/List.jsx
+++ client/src/components/Main/Landing/List/List.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Card from '../List/Card';
+
 
 function List() {
+
+  const [landings, setLandings] = useState([]);
+  //const [id, setId] = useState("");
+
+
+  useEffect(() => {
+    const getLandings = async () => {
+      const resp = await fetch("http://localhost:3000/api/astronomy/landings");
+      const data = await resp.json();
+      setLandings(data);
+    }
+    getLandings();
+  }, [landings]);
+
   return (
     <div className='list'>
-      
+
+{landings.map(landing => (<Card 
+          value={landing}
+          key={crypto.randomUUID()} 
+        />
+      ))}
+
     </div>
-  )
+  );
 }
 
 export default List
